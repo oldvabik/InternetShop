227d723cb13dc99ca003a5ea3e692f44854972fa
Maksim Vabishchevich <oldvabik@gmail.com>
2025-03-24 03:54:01 +0300

    update for checkstyle

--- src/main/java/com/oldvabik/internetshop/controller/CategoryController.java
+++ src/main/java/com/oldvabik/internetshop/controller/CategoryController.java
@@ -7,7 +7,6 @@ import com.oldvabik.internetshop.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
